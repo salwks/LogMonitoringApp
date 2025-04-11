78dc4a9d466279c2da2c7266c00edff191a5aa2b
DESKTOP-PF07HU7\jhsin <jhsin@genoray.com>
2025-04-11 17:10:48 +0900

    초기 커밋.

---
+++ Shell/AppShell.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System.Windows.Controls;
+using LogMonitoringApp.Shell.Navigation; // TopNavBar를 위한 네임스페이스
+
+namespace LogMonitoringApp.Shell
+{
+    public partial class AppShell : UserControl
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+            // TopNavBarControl의 TabButtonClicked 이벤트 구독
+            TopNavBarControl.TabButtonClicked += TopNavBarControl_TabButtonClicked;
+        }
+
+        private void TopNavBarControl_TabButtonClicked(object sender, int tabIndex)
+        {
+            MainTabControl.SelectedIndex = tabIndex;
+        }
+    }
+}
---
+++ Shell/Navigation/TopNavBar.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace LogMonitoringApp.Shell.Navigation
+{
+    public partial class TopNavBar : UserControl
+    {
+        // 이벤트를 nullable로 선언
+        public event EventHandler<int>? TabButtonClicked;
+
+        public TopNavBar()
+        {
+            InitializeComponent();
+        }
+
+        private void OnRadioTabChecked(object sender, RoutedEventArgs e)
+        {
+            if (sender is RadioButton rb && int.TryParse(rb.Tag?.ToString(), out int index))
+            {
+                TabButtonClicked?.Invoke(this, index);
+            }
+        }
+    }
+}
